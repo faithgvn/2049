8437e7efeb755615bee5b3ce98f3b3af8e9f6b83
faithgvn <faithgvn@gmail.com>
2024-02-24 23:44:53 +0300

    add board

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[])
 {
-    InitWindow(800, 600, "2049 by faithgvn");
+    InitWindow(800, 800, "2049 by faithgvn");
 
     while (!WindowShouldClose()) {
         /* Read Keyboard Inputs */
@@ -14,7 +14,14 @@ int main(int argc, char* argv[])
         /* Draw everthing */
 
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground(WHITE);
+        DrawRectangle(100, 100, 600, 600, GRAY);
+        DrawLine(250, 100, 250, 700, WHITE);
+        DrawLine(400, 100, 400, 700, WHITE);
+        DrawLine(550, 100, 550, 700, WHITE);
+        DrawLine(100, 250, 700, 250, WHITE);
+        DrawLine(100, 400, 700, 400, WHITE);
+        DrawLine(100, 550, 700, 550, WHITE);
         EndDrawing();
     }
 
